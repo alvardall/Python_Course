fdbf2e7c6a961a5bb9d879625e6dcec3a0fbc550
alvardall <alvardtel@gmail.com>
2023-03-07 00:40:43 +0400

    added 1.py file

---
+++ Lessons/Lesson2/lesson2.py
@@ -0,0 +1,6 @@
+print("Extensions. Install Python Extension. First, click the Extensions tab.Second, type the pythonkeyword on the search input.Third, click the Python extension. It'll show detailed information on the right pane. Finally, click the Install button to install the Python extension")
+print("To turn on or turn off the Auto Save feature in Visual Studio Code, we  go to File and click on Auto Save from the drop-down menu")
+print("We did an exercise.Given two sides of a triangle, we had to calculate the diagonal")
+print("Python Interpreter")
+print("Interpreter vs compilers")
+print("Zen of python")
